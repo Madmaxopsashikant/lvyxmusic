4ebe64968ac476318d9947ee3c7bb887efe3da65
Madmaxopsashikant <144993064+Madmaxopsashikant@users.noreply.github.com>
2023-12-12 07:56:00 +0530

    Update play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -69,14 +69,14 @@ def stream_markup_timer(_, chat_id, played, dur):
                  [
             InlineKeyboardButton(
 
-                text="ᴅᴇᴠ 💕",
+                text="🥀𝐃ᴇᴠᴇʟᴏᴘᴇʀ🥀",
 
                 url=f"t.me/{OWNER_USERNAME}",
 
             ),
             InlineKeyboardButton(
 
-                text="💌 ɢʀᴏᴜᴘ",
+                text="💫𝐆ʀᴏᴜᴘ✨",
 
                 url=f"{SUPPORT_CHAT}",
 
@@ -99,14 +99,14 @@ def stream_markup(_, chat_id):
                  [
             InlineKeyboardButton(
 
-                text="ᴅᴇᴠ 💕",
+                text="🥀𝐃ᴇᴠᴇʟᴏᴘᴇʀ🥀",
 
                 url=f"t.me/{OWNER_USERNAME}",
 
             ),
             InlineKeyboardButton(
 
-                text="💌 ɢʀᴏᴜᴘ",
+                text="💫𝐆ʀᴏᴜᴘ✨",
 
                 url=f"{SUPPORT_CHAT}",
 
